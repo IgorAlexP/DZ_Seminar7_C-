3ce6b9a14acfd4206dc659215dc73a26228c6ade
Igor Popkov <i.popkov@CPSURAL.LOCAL>
2022-12-07 16:23:28 +0500

    задача 50

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4 */
+Console.Write("Введите количество столбцов m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите номер строки массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите номер столбца массива: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+PrintArray(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(a == i && b == j)
+            {
+                Console.Write($"Элемент массива с номеромстроки {a} и номером столбца {b} => {matrix[i-1, j-1]}");
+            }
+        }
+    Console.WriteLine();
+    }
+if(a >= matrix.GetLength(0) || b >= matrix.GetLength(1))
+        {
+            Console.Write("Такого элемента в массиве нет");
+        }
+
+void PrintArray(int[,] matr)          //метод создания массива чисел
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+            Console.Write($"{matr[i, j]} ");
+        
+        }
+    Console.WriteLine();
+    }
+
+
+}
+
