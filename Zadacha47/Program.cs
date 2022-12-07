d8d1428c9486baf514263417ce3da0a7ee0d81ca
Igor Popkov <i.popkov@CPSURAL.LOCAL>
2022-12-07 13:55:50 +0500

    задача 47

---
+++ Zadacha47/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+Console.Write("Введите количество столбцов m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)          //метод создания массива чисел
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(10, 100);
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
